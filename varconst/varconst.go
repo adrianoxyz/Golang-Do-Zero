51cbd66c05c6562754ab70350bd95d93cbe7600d
adrianoxyz <adrianoxyz@gmail.com>
2024-10-30 18:05:10 -0300

    Fundamentos da linguagem Go

---
+++ varconst/varconst.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Variáveis
+
+	var nome = "Adriano"
+	fmt.Println(nome)
+
+	idade := 4
+	fmt.Println(idade)
+
+	f := "apple"
+	fmt.Println(f)
+
+	real := 3.33
+	fmt.Println(real)
+
+}
